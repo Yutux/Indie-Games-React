22f3cfb026e6b803a2125367e83e0c7a0da43bca
Zogoury <david.zogoury125@gmail.com>
2022-11-22 09:39:08 +0100

    contact us

--- frontend/src/Conponents/ContactusForm.js
+++ frontend/src/Conponents/ContactusForm.js
@@ -5,70 +5,101 @@ import Container from "@mui/material/Container";
 import InputLabel from "@mui/material/InputLabel";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
-//import Input from "@mui/material/Input";
-//import OutlinedInput from "@mui/material/OutlinedInput";
-//import FormControl from "@mui/material/FormControl";
+import { useForm, ValidationError } from '@formspree/react';
 
 
 
 export default function ContactusForm() {
+
+    const [state, handleSubmit] = useForm("xdojbqpy");
+    if(state.succeeded){
+        return <p>thanks</p>
+    }
   return (
     <div>
         <Container>
             <div className="contactUform">
-            <Grid container spacing={3} xs={12}>
-                <Grid item={true} xs={6}>
-                    <InputLabel> </InputLabel>
-                    <TextField
-                        id="component-outlined"
-                        size="small"
-                        label="First Name"
-                        variant="outlined"
-                        fullWidth
-                    />
-                </Grid>
-                <Grid item={true} xs={6}>
-                    <InputLabel></InputLabel>
-                    <TextField
-                        id="outlined-basic"
-                        size="small"
-                        label="Last Name"
-                        variant="outlined"
-                        fullWidth
-                    />
-                </Grid>
-                <Grid item={true} xs={12}>
-                    <InputLabel></InputLabel>
-                    <TextField
-                        id="outlined-basic"
-                        size="small"
-                        label="Email"
-                        variant="outlined"
-                        fullWidth
-                    />
-                </Grid>
-                <Grid item={true} xs={12}>
-                <TextField
-                    id="outlined-multiline-static"
-                    label="Message"
-                    multiline
-                    rows={4}
-                    variant="outlined"
-                    fullWidth
-                />
-                </Grid>
-                <Grid xs={12} item={true}>
-                <Button
-                    color="secondary"
-                    disableElevation
-                    variant="contained"
-                    fullWidth
-                >
-                    {" "}
-                    Submit{" "}
-                </Button>
-                </Grid>
-            </Grid>
+                <form onSubmit={handleSubmit}>
+                    <Grid container spacing={3} xs={12}>
+                        <Grid item={true} xs={6}>
+                            <InputLabel htmlFor='First Name'></InputLabel>
+                            <TextField
+                                id="outlined-basic"
+                                size="small"
+                                label="First Name"
+                                type='first name'
+                                name='first name'
+                                fullWidth
+                            />
+                            <ValidationError 
+                            prefix="First name" 
+                            field="First name"
+                            errors={state.errors}
+                            />
+                        </Grid>
+                        <Grid item={true} xs={6}>
+                            <InputLabel htmlFor='Last Name'></InputLabel>
+                            <TextField
+                                id="outlined-basic"
+                                size="small"
+                                type='last name'
+                                name='last name'
+                                label="Last Name"
+                                fullWidth
+                            />
+                            <ValidationError 
+                            prefix="Last name" 
+                            field="Last name"
+                            errors={state.errors}
+                            />
+                        </Grid>
+                        <Grid item={true} xs={12}>
+                            <InputLabel htmlFor='Email'></InputLabel>
+                            <TextField
+                                id="outlined-basic"
+                                size="small"
+                                type='email'
+                                name='email'
+                                label="Email"
+                                fullWidth
+                            />
+                            <ValidationError 
+                            prefix="email" 
+                            field="email"
+                            errors={state.errors}
+                            />
+                        </Grid>
+                        <Grid item={true} xs={12}>
+                            <TextField
+                                id="outlined-multiline-static"
+                                label="Message"
+                                name='message'
+                                multiline
+                                rows={4}
+                                variant="outlined"
+                                fullWidth
+                            />
+                            <ValidationError 
+                                prefix="Message" 
+                                field="message"
+                                errors={state.errors}
+                                />
+                        </Grid>
+                        <Grid xs={12} item={true}>
+                            <Button
+                                color="secondary"
+                                type='submit'
+                                disabled={state.submitting}
+                                disableElevation
+                                variant="contained"
+                                fullWidth
+                            >
+                                {" "}
+                                Submit{" "}
+                            </Button>
+                        </Grid>  
+                    </Grid>
+                </form>
             </div>
       </Container>
     </div>
--- frontend/src/Conponents/Footer.js
+++ frontend/src/Conponents/Footer.js
@@ -12,12 +12,12 @@ import "../index.css";
 export const Footer = () => {
 
   function changeEnter(e){
-    e.target.style.color = 'white';
-    e.target.style.background = 'black';
+    e.target.style.color = 'black';
+    e.target.style.background = 'white';
     e.target.style.textDecoration = 'underline';
   };
   function changeLeave(e){
-    e.target.style.color = 'black';
+    e.target.style.color = 'white';
     e.target.style.background = 'none';
     e.target.style.textDecoration = 'none';
   };
@@ -25,7 +25,7 @@ export const Footer = () => {
   return (
     <div className="footer">
       <div>
-        <div className="footer-content">
+        <div  className="footer-content">
           <h3
             style={{
               fontFamily: "Trebuchet MS",
@@ -40,7 +40,7 @@ export const Footer = () => {
               <b>Company</b>
               <p>About</p>
               <p>Blog</p>
-              <p><Link onMouseEnter={changeEnter} onMouseLeave={changeLeave} style={{textDecoration: 'none', color: 'black' }} to={"/contactus"}> Contact Us </Link></p>
+              <p><Link onMouseEnter={changeEnter} onMouseLeave={changeLeave} style={{textDecoration: 'none', color: 'white' }} to={"/contactus"}> Contact Us </Link></p>
             </div>
             <div>
               <b>For Gamers</b>
@@ -49,10 +49,10 @@ export const Footer = () => {
             </div>
             <div>
               <b>For You</b>
-              <p><Link onMouseEnter={changeEnter} onMouseLeave={changeLeave} style={{textDecoration: 'none', color: 'black' }} to={"/Term&Condition"}> Terms of service </Link></p>
-              <p><Link onMouseEnter={changeEnter} onMouseLeave={changeLeave} style={{textDecoration: 'none', color: 'black' }} to={"/Refund"}> Refund </Link></p>
-              <p><Link onMouseEnter={changeEnter} onMouseLeave={changeLeave} style={{textDecoration: 'none', color: 'black' }} to={"/disclaimer"}> Disclaimer </Link></p>
-              <p><Link onMouseEnter={changeEnter} onMouseLeave={changeLeave} style={{textDecoration: 'none', color: 'black' }} to={"/PrivacyPolicy"}> Privacy Policy </Link></p>
+              <p><Link onMouseEnter={changeEnter} onMouseLeave={changeLeave} style={{textDecoration: 'none', color: 'white' }} to={"/Term&Condition"}> Terms of service </Link></p>
+              <p><Link onMouseEnter={changeEnter} onMouseLeave={changeLeave} style={{textDecoration: 'none', color: 'white' }} to={"/Refund"}> Refund </Link></p>
+              <p><Link onMouseEnter={changeEnter} onMouseLeave={changeLeave} style={{textDecoration: 'none', color: 'white' }} to={"/disclaimer"}> Disclaimer </Link></p>
+              <p><Link onMouseEnter={changeEnter} onMouseLeave={changeLeave} style={{textDecoration: 'none', color: 'white' }} to={"/PrivacyPolicy"}> Privacy Policy </Link></p>
             </div>
             <div>
               <b>Social links</b>
